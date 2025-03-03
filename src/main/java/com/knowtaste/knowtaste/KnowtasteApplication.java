93f3ac9030231d1e7c6d2668a556df5ef987c162
xingzuozhou <xingzuozhou@gmail.com>
2025-03-03 23:40:53 +0800

    Initial commit

---
+++ src/main/java/com/knowtaste/knowtaste/KnowtasteApplication.java
@@ -0,0 +1,13 @@
+package com.knowtaste.knowtaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowtasteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowtasteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knowtaste/knowtaste/KnowtasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowtaste.knowtaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowtasteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
